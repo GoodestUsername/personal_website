263860a7986a0285d0dbcee4de1dcdfc91d49226
GoodestUsername <ericdongcannn@hotmail.com>
2022-12-28 23:56:47 -0800

    added projevts section to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
 import SiteNavbar from 'components/SiteNavbar';
 import { Container } from 'react-bootstrap';
 import AboutMeSection from 'static/AboutMeSection';
+import CareerSection from 'static/CareerSection';
+import ProjectSection from 'static/ProjectSection';
+
 
 function App() {
   return (
@@ -9,6 +12,8 @@ function App() {
       <Container className="d-flex flex-column justify-content-center"
         style={{maxWidth: "1000px"}}>
         <AboutMeSection/>
+        <ProjectSection/>
+        <CareerSection/>
       </Container>
     </div>
   );
